7f2a876569c6b5e4c3b7d1bf2bf1faae8ef0ee54
CHRIS <chris@>
2018-07-13 18:48:33 -0700

    Fixed choco detection, reload path after software installs

--- Install-Software.ps1
+++ Install-Software.ps1
@@ -1,27 +1,44 @@
 
-Try
+Param
+(
+    [String]$Inventory = '.\Install-Software.json',
+    [Switch]$SkipChoco,
+    [Switch]$SkipCode
+)
+
+
+$Software = Get-Content -Path $Inventory | ConvertFrom-Json
+
+If ( -not $SkipChoco )
 {
-    $Choco = Get-Command -Name 'choco'
-}
-Catch
-{
-    Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) # Dangerous!
+    $Chocolatey = $Null
+    $Chocolatey = Get-Command -Name 'choco' -ErrorAction SilentlyContinue
+
+    If ( -not $Chocolatey )
+    {
+        Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
+        Invoke-Expression -Command (  (New-Object -TypeName System.Net.WebClient).DownloadString( 'https://chocolatey.org/install.ps1' ) ) # Dangerous!
+    }
+
+    Get-Member -InputObject $Software.'choco-install' -MemberType NoteProperty | ForEach-Object {
+        $Version = $_.Definition.Split('=')[1]
+        If ( $Version -notlike 'latest' )
+        {
+            choco install $_.Name --version $Version --yes
+            choco pin add --name $_.Name --version $Version
+        }
+        Else
+        {
+            choco install $_.Name --yes
+        }
+    }
+    
+    $env:PATH = Get-ItemProperty -Path 'HKLM:System\CurrentControlSet\Control\Session Manager\Environment' -Name Path
 }
 
-$Software = Get-Content -Path .\Install-Software.json | ConvertFrom-Json
-Get-Member -InputObject $Software.'choco-install' -MemberType NoteProperty | ForEach-Object {
-    $Version = $_.Definition.Split('=')[1]
-    If ( $Version -notlike 'latest' )
-    {
-        choco install $_.Name --version $Version --yes
-        choco pin add --name $_.Name --version $Version
+If ( -not $SkipCode )
+{
+    $Software.'code-install' | ForEach-Object {
+        code --install-extension $_
     }
-    Else
-    {
-        choco install $_.Name --yes
-    }
-}
-
-Get-Member -InputObject $Software.'vscode-install' -MemberType NoteProperty | ForEach-Object {
-    code --install-extension $_
 }
